bbdaa89955cbd3d3edf8f0575ff3502664eb55ba
Myrrolinz <mildred1195564913@gmail.com>
2024-11-06 18:34:43 -0800

    initial try

---
+++ georgia_energy_scraper.py
@@ -0,0 +1,11 @@
+from kubra_scraper import KubraScraper
+
+
+class AustinEnergyScraper(KubraScraper):
+    owner = "Myrrolinz"
+    repo = "georgia-energy-outage"
+    filepath = "outages.json"
+
+    instance_id = "7b38c047-7950-444b-a25c-9b3e5ab986eb"
+    view_id = "67b44af5-3847-4ca3-9f4e-9190aac343d6"
+    
